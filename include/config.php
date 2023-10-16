9883b2892e76039b7fb3a7a7208f9e849b545172
abdullah ibrahim Mohamed <144289227+20abdullahib@users.noreply.github.com>
2023-10-16 22:56:10 +0300

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,9 @@
+<?php
+$server = "sql9.freesqldatabase.com";
+$username = "sql9653847";
+$password = "TImg8dhATT";
+$database = "sql9653847";
+
+$conn = new mysqli($server, $username, $password, $database);
+
+?>
